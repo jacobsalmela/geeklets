e5e76e847ddda72938233efc3874bab824bf2a2a
jacobsalmela <jacob.salmela@gmail.com>
2014-08-08 09:09:32 -0500

    removed echoes so it runs in the bg

--- speak-when-connection-is-active.sh
+++ speak-when-connection-is-active.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #----------AUTHOR------------
-	# Jacob Salmela
-	# 11 March 2014
+# Jacob Salmela
+# 11 March 2014
 
 #---------DESCRIPTION--------
-	# You Mac speaks to you when an Ethernet cable is plugged in or out
+# You Mac speaks to you when an Ethernet cable is plugged in or out
 
 #----------VARIABLES---------
 ethernetDevice=$(/usr/sbin/networksetup -listallhardwareports | awk "/Hardware Port: Ethernet/,/Device/" | awk 'NR==2' | cut -d " " -f 2)
@@ -16,13 +16,9 @@ interfaceDown=$(ifconfig $ethernetDevice | grep 'inactive' | awk '{print $2}')
 # When you plug in or unplug an Ethernet cable, the computer will verbally tell you
 # Works great for quickly testing network ports without having to open Network Preferences and watch the jellies
 
-#------------------------------		
 #-------BEGIN SCRIPT-----------
-#------------------------------	
 if [ "$interfaceStatus" = "active" ]; then
-	echo -e Ethernet adapter is $interfaceStatus.
 	say -v "Samantha" "Your hardline is $interfaceStatus" &> /dev/null
 else
-	echo -e Ethernet adapter is $interfaceDown.
 	say -v "Samantha" "Your hardline is $interfaceDown" &> /dev/null
 fi
