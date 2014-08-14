7239320228d05741db1d5908765829cc67b1e652
jacobsalmela <jacob.salmela@gmail.com>
2014-08-13 19:57:54 -0500

    simplified syntax

--- detect-screen-sharing.sh
+++ detect-screen-sharing.sh
@@ -7,19 +7,9 @@ red="\x1b[031m"
 green="\x1b[032m"
 yellow="\x1b[033m"
 
-echo "${bold}REMOTE CONNECTIONS${end}"
-userConnected=$(cat /var/log/system.log | grep -i "screensharingd" | grep -i "Authentication: SUCCEEDED" | tail -n1 | awk '{print $11}')
-originIP=$(cat /var/log/system.log | grep -i "screensharingd" | grep -i "Authentication: SUCCEEDED" | tail -n1 | awk '{print $15}')
-timeConnected=$(cat /var/log/system.log | grep -i "screensharingd" | grep -i "Authentication: SUCCEEDED" | tail -n1 | awk '{print $3}')
-
 port5900detected=$(netstat -n | grep 5900)
 if [[ -z "$port5900detected" ]];then
-	echo "\t✅ Nominal"
+    echo "\t✅ VNC"
 else
-	echo "\t🔴 ${red}Remote session detected!${end}"
-	who
-	logEntries=$(cat /var/log/system.log | grep -i screensharingd | tail -n 3 | awk '{print $1, $2, $3, $4, $11, $15}')
-	echo "${red}TAIL LOG ENTRIES${end}"
-	echo "$logEntries"
-	
+    echo "\t🔴 ${red}VNC!${end}"
 fi
--- detect-ssh-session.sh
+++ detect-ssh-session.sh
@@ -1,4 +1,4 @@
-# Detect remote sessions
+# Detect ssh sessions
 # Colors
 end="\x1b[0m"
 bold="\x1b[001m"
@@ -7,17 +7,9 @@ red="\x1b[031m"
 green="\x1b[032m"
 yellow="\x1b[033m"
 
-echo "${bold}REMOTE CONNECTIONS${end}"
-
-port22detected=$(lsof -i | grep -i ssh)
+port22detected=$(netstat -p tcp | grep ssh)
 if [[ -z "$port22detected" ]];then
-	echo "\t✅ Nominal"
+    echo "\t✅ SSH"
 else
-	echo "\t🔴 ${red}Remote session detected!${end}"
-	sshSessions=$(lsof -i | grep -i ssh | awk '{print $3, $9, $10}')
-	echo "$sshSessions"
-	logEntries=$(cat /var/log/system.log | grep -i sshd | grep -i "Accepted keyboard-interactive/pam for" | tail -n 3 | awk '{print $1, $2, $3, $4, $9, $11}')
-	echo "${red}TAIL LOG ENTRIES${end}"
-	echo "$logEntries"
-	
+    echo "\t🔴 ${red}SSH!${end}"
 fi
